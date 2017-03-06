69fa58214a60e623930b5430ad299fe13054def6
mariansurdu <surdu_marian94@yahoo.com>
2017-03-06 19:43:38 +0200

    Added side menu

--- test/ionic2-tutorial-github/src/app/app.module.ts
+++ test/ionic2-tutorial-github/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Page1,
+    Page2
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Page1,
+    Page2
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
