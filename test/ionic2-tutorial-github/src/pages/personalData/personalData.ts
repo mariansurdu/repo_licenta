f71fc188ed57ac21de087553d7b2159aa22f7ea6
mariansurdu <surdu_marian94@yahoo.com>
2017-03-24 17:38:56 +0200

    Commit

--- test/ionic2-tutorial-github/src/pages/personalData/personalData.ts
+++ test/ionic2-tutorial-github/src/pages/personalData/personalData.ts
@@ -9,31 +9,69 @@ import {PersonalDataService} from "./personalData.service";
 })
 export class PersonalData {
     data:any;
+  labels:any=[];
+  arrayGas:any=[];
+  arrayMetan:any=[];
+  arrayNH3:any=[];
+  arrayCO:any=[];
+  arrayCO2:any=[];
+  arrayAirQuality:any=[];
+  timeStarts:any=[];
   constructor(public navCtrl: NavController,public personalDataService:PersonalDataService) {
-
+      this.timeStarts=new Date();
   }
   public lineChartType:string = 'line';
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public days:any=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
+
+
+  public barChartLabels:string[] = this.labels;
   public barChartType:string = 'bar';
   public barChartLegend:boolean = true;
 
   public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 34, 55, 40], label: 'Gas'},
-    {data: [10, 48, 40, 19, 86, 27, 90], label: 'Metan'},
-    {data: [12, 48, 40, 19, 12, 27, 90], label: 'NH3'},
-    {data: [60, 48, 40, 19, 22, 27, 90], label: 'CO'},
-    {data: [35, 48, 40, 19, 52, 27, 90], label: 'CO2'}
+    {data: [0,0,0,0,0,0,0], label: 'Gas'},
+    {data: [0,0,0,0,0,0,0], label: 'Metan'},
+    {data: [0,0,0,0,0,0,0], label: 'NH3'},
+    {data: [0,0,0,0,0,0,0], label: 'CO'},
+    {data: [0,0,0,0,0,0,0], label: 'CO2'},
+    {data: [0,0,0,0,0,0,0], label: 'Air quality'}
   ];
 
 
   ngOnInit() {
   this.personalDataService
     .getData(1)
-    .subscribe(value => this.data = value , error => alert(error) );
+    .subscribe((res)=>{
+      console.log(res);
+      this.data=res[0].dataUser[0].airquality;
+      for (var i=0;i<res[0].dataUser.length;i++)
+      {
+        this.arrayAirQuality.push(res[0].dataUser[i].airquality);
+        this.arrayMetan.push(res[0].dataUser[i].metan);
+        this.arrayCO.push(res[0].dataUser[i].co);
+        this.arrayCO2.push(res[0].dataUser[i].co2);
+        this.arrayNH3.push(res[0].dataUser[i].nh3);
+        this.arrayGas.push(res[0].dataUser[i].gas);
+        this.labels.push(this.days[new Date(res[0].dataUser[i].date).getDay()-2]);
+
+      }
+      this.barChartData=[
+        {data: this.arrayGas, label: 'Gas'},
+        {data: this.arrayMetan, label: 'Metan'},
+        {data: this.arrayNH3, label: 'NH3'},
+        {data: this.arrayCO, label: 'CO'},
+        {data: this.arrayCO2, label: 'CO2'},
+        {data: this.arrayAirQuality, label: 'Air quality'}
+      ];
+      console.log(this.barChartData);
+      this.barChartLabels=["2","3"];
+      console.log(this.barChartLabels);
+
+    });
 }
 
 
